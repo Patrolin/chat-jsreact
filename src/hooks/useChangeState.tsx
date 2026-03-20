8a03a772a085b75a61fac01fadf10802d5aa7842
Patrolin <patrik@balas.cz>
2026-03-20 20:36:53 +0100

    c

--- src/hooks/useChangeState.tsx
+++ src/hooks/useChangeState.tsx
@@ -1,15 +1,15 @@
 import { useCallback, useReducer } from "react";
 
-export function useChangeState<T extends Record<string, any>>(defaultState: T): [T, (diff: Partial<T>) => void] {
+export function useChangeState<T extends Record<string, any>>(defaultState: T): [T, (diff: Partial<T>, rerender?: boolean) => void] {
   /* NOTE: React doesn't export a rerender function, so we do this workaround */
-  const [state, rerender] = useReducer((v) => v, defaultState);
+  const [state, forceRerender] = useReducer((v) => v, defaultState);
   const changeState = useCallback(
-    (diff: Partial<T>) => {
+    (diff: Partial<T>, rerender = true) => {
       if (Object.keys(diff).every((k) => Object.is(diff[k], state[k]))) return;
       Object.assign(state, diff);
-      rerender();
+      if (rerender) forceRerender();
     },
-    [rerender]
+    [forceRerender]
   );
   return [state, changeState];
 }
--- src/hooks/useGetRequest.tsx
+++ src/hooks/useGetRequest.tsx
@@ -1,33 +1,79 @@
+import { useCallback } from "react";
 import { useChangeState } from "./useChangeState";
 
-type Options<T, D> = {
+// cache requests
+type CachedRequest<T = any> = {
+  prevFetchedOn: any[] | undefined;
+  response: T;
+};
+const responseCache: Record<string, CachedRequest | undefined> = {};
+function getCachedResponse<T>(cacheId: string | undefined, refetchOn: any[] | undefined): T | undefined {
+  if (cacheId == null) return;
+  const cachedRequest = responseCache[cacheId as string];
+  if (dependenciesDiffer(cachedRequest?.prevFetchedOn, refetchOn)) return undefined;
+  return cachedRequest!.response;
+}
+function setCachedRequest<T>(cacheId: string | undefined, refetchOn: any[] | undefined, response: T) {
+  if (cacheId == null) return;
+  responseCache[cacheId] = { prevFetchedOn: refetchOn, response };
+}
+
+// get request
+function dependenciesDiffer(a: any[] | undefined, b: any[] | undefined) {
+  return a == null || b == null || a.length !== b.length || !b.every((v, i) => Object.is(v, a[i]));
+}
+type UseGetRequestOptions<T, D> = {
   defaultValue?: D;
   fetch: (abortSignal: AbortSignal) => Promise<T>;
-  disabled?: boolean;
   refetchOn?: any[];
+  onError?: (errorOrResponse: any) => void;
+  disabled?: boolean;
+  cacheId?: string;
 };
-export function useGetRequest<T, D extends T | undefined>(options: Options<T, D>): [boolean, D, boolean] {
-  const { defaultValue, fetch, disabled, refetchOn = [] } = options;
+export function useGetRequest<T, D extends T | undefined>(options: UseGetRequestOptions<T, D>): [boolean, D, () => Promise<D>, boolean] {
+  // state
   const [state, changeState] = useChangeState({
-    data: defaultValue,
+    data: options.defaultValue as D,
     loading: true,
     initiallyLoading: true,
-    prevRefetchOn: null as any[] | null,
+    prevRefetchOn: undefined as any[] | undefined,
     abortController: null as AbortController | null,
+    options,
   });
-  if (disabled) {
-    state.loading = false;
-    state.initiallyLoading = false;
-  } else if (state.prevRefetchOn == null || refetchOn == null || refetchOn.some((v, i) => !Object.is(v, state.prevRefetchOn?.[i]))) {
-    if (state.abortController != null) state.abortController.abort();
-    const abortController = new AbortController();
-    state.loading = true;
-    state.prevRefetchOn = refetchOn;
-    state.abortController = abortController;
-    Promise.try(() => fetch(abortController.signal)).then((data) => {
-      changeState({ data: data as D, loading: false, initiallyLoading: false, abortController: null });
-    });
+
+  // callbacks
+  const _fetchAndRerender = useCallback(
+    (rerender: boolean) => {
+      const { fetch, refetchOn = [], onError, cacheId } = state.options;
+      if (state.abortController != null) state.abortController.abort();
+      const abortController = new AbortController();
+      changeState({ loading: true, prevRefetchOn: refetchOn, abortController }, rerender);
+      return Promise.try(() => fetch(abortController.signal))
+        .then((data: T) => {
+          setCachedRequest(cacheId, refetchOn, data);
+          changeState({ data: data as D, loading: false, initiallyLoading: false, abortController: null }, true);
+          return data as D;
+        })
+        .catch((error) => {
+          onError?.(error);
+          return state.data;
+        });
+    },
+    [state, changeState]
+  );
+  const refetch = useCallback(() => _fetchAndRerender(true), [_fetchAndRerender]);
+
+  // update state without rerendering, and start the fetch if necessary
+  const { refetchOn = [], cacheId } = options;
+  if (options.disabled) {
+    changeState({ loading: false, initiallyLoading: false }, false);
+  } else if (dependenciesDiffer(state.prevRefetchOn, refetchOn)) {
+    const cachedResponse = getCachedResponse<T>(cacheId, refetchOn);
+    if (cachedResponse != null) {
+      changeState({ data: cachedResponse as D, loading: false, initiallyLoading: false }, false);
+    } else {
+      _fetchAndRerender(false);
+    }
   }
-  if (disabled) state.loading = false;
-  return [state.loading, state.data as D, state.initiallyLoading];
+  return [state.loading, state.data, refetch, state.initiallyLoading];
 }
